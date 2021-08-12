91629c1fc4162dfed87e6f691fb0f4af26a591f2
최승희 <csh1002sh@gmail.com>
2021-08-12 18:58:08 +0900

    no message

--- __init__.py
+++ __init__.py
@@ -2,17 +2,20 @@ from ramen.nongsim import *
 from study.ramen_test.samyang import *
 from study.ramen_test.ottogi import *
 # from ramen_test.paldo import *
+# from study.csv.daily_sise_csvfile import *
+from study.stock.allocation_csvfile import kor_allocation
 
 class Main():
     def __init__(self):
-        print("신라면------------------------------------")
-        self.nongsim = nongsim()
-        print("불닭 영양성분-------------------------------")
-        self.samyang = samyang()
-        print("진라면 열량--------------------------------")
-        self.jin = jin()
+        # self.nongsim = nongsim()
+        # print("불닭 영양성분-------------------------------")
+        # self.samyang = samyang()
+        # print("진라면 열량--------------------------------")
+        # self.jin = jin()
         # print("팔도비빔면 열량--------------------------------")
         # self.paldo = paldo()
+        # self.kor_daily_sise = kor_daily_sise()
+        self.kor_allocation = kor_allocation()
 
 
 if __name__ == "__main__":
--- study/stock/allocation_csvfile.py
+++ study/stock/allocation_csvfile.py
@@ -4,106 +4,60 @@ import csv
 
 class kor_allocation():
     def __init__(self):
-        self.path = "/Users/choeseunghui/Documents/"
+        self.path = "/Users/choeseunghui/Documents"     # 파일 다운로드 경로
 
-        self.name = ""
-        self.revenue = ""
+        self.name = ""      # 1p 종목명 이름들
+        self.revenue = ""       # 1p 배당수익률
 
-        self.namesList = []
-        self.revenueList = []
+        self.name_List = []     # 1p 종목명 이름 리스트로 묶음
+        self.revenue_List = []      # 1p 배당수익률 리스트로 묶음
 
-        self.nn = []
-        self.rr = []
 
-        self.revenue_code25 = []
-
-        self.name25 = ""
-        self.revenue25 = ""
-
-        self.names_List25 = []
-        self.revenue_List25 = []
-
-        self.nn25 = []
-        self.rr25 = []
-
-        # self.name_data = []
-        # self.revenue_data = []
-        #
         self.kor_allocation()
 
+
     def kor_allocation(self):
-
-        pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-
-        # pages = [1, 2]
-
-        revenue_code = [16, 28, 40, 52, 64, 100, 112, 124, 136, 148, 184, 196, 208, 220, 232,
-                        268, 280, 292, 304, 316, 352, 364, 376, 388, 400, 436, 448, 460, 472, 484,
-                        520, 532, 544, 556, 568, 604, 616, 628, 640, 652, 688, 700, 712, 724, 736,
-                        772, 784, 796, 808, 820]
+        # print(self.path)
+        pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
         for page in pages:
+
             url = f"https://finance.naver.com/sise/dividend_list.nhn?&page={str(page)}"
             reponses = requests.get(url)
-            soup = BeautifulSoup(reponses.content, 'html.parser', from_encoding="cp949")
+            soup = BeautifulSoup(reponses.content, 'html.parser', from_encoding='cp949')
 
-            self.namesList = []
+            self.name_List = []
             self.name = soup.find("table", {"class": "type_1 tb_ty"}).find_all("a")[8:]
             for n in self.name:
                 # print(n.text)
-                self.namesList.append(n.text)
+                self.name_List.append(n.text)
+            # print(self.name_List)
 
-            self.revenueList = []
-            for r in revenue_code:
-                self.revenue = soup.find("table", {"class": "type_1 tb_ty"}).find_all("td")[r].text
-                self.revenueList.append(self.revenue)
+            self.revenue_List = []
 
-            for self.nn, self.rr in zip(self.namesList, self.revenueList):
-                print(self.nn, ":", self.rr)
+            num = [16, 28, 40, 52, 64, 100, 112, 124, 136, 148, 184, 196, 208, 220, 232,
+                    268, 280, 292, 304, 316, 352, 364, 376, 388, 400, 436, 448, 460, 472, 484,
+                    520, 532, 544, 556, 568, 604, 616, 628, 640, 652, 688, 700, 712, 724, 736,
+                    772, 784, 796, 808, 820]
 
-        
+            for i in num:
+                self.revenue = soup.find("table", {"class": "type_1 tb_ty"}).find_all("td")[i].text
+                # print(self.revenue)
+                self.revenue_List.append(self.revenue)
+            # print(self.revenue_List)
 
-        # 25p
-        # url = "https://finance.naver.com/sise/dividend_list.nhn?&page=25"
-        # reponses = requests.get(url)
-        # soup = BeautifulSoup(reponses.content, 'html.parser', from_encoding='cp949')
-        #
-        # self.revenue_code25 = [16, 28, 40, 52, 64, 100, 112, 124, 136, 148, 184, 196, 208, 220, 232,
-        #                        268, 280, 292, 304, 316, 352, 364]
-        #
-        # self.names_List25 = []
-        # self.name25 = soup.find("table", {"class": "type_1 tb_ty"}).find_all("a")[8:]
-        # for n25 in self.name25:
-        #     # print(n25.text)
-        #     self.names_List25.append(n25.text)
-        #
-        # self.revenue_List25 = []
-        # for i in self.revenue_code25:
-        #     self.revenue25 = soup.find("table", {"class": "type_1 tb_ty"}).find_all("td")[i]
-        #     self.revenue_List25.append(self.revenue25.text)
-        #
-        # for self.nn25, self.rr25 in zip(self.names_List25, self.revenue_List25):
-        #     print(self.nn25, self.rr25)
-        #
-        # # self.revenue_data = self.rr + self.rr25
-        # # self.name_data = self.nn + self.nn25
-        # # print(self.revenue_data)
-        # # print(self.name_data)
+            for name_all, revenue_all in zip(self.name_List, self.revenue_List):
+                print(name_all, revenue_all)
 
-        write_file = open("/Users/choeseunghui/Documents/stock_allocation.csv", 'w', newline="")
-        writer = csv.writer(write_file)
-        writer.writerow([
-            "종목명", "배당수익률(%)"
-            ])
-
-        for num in range(50):
-            writer.writerow([
-                self.namesList[num],
-                self.revenueList[num]
-
-                # self.revenue_data[num],
-                # self.name_data[num]
+            writer_file = open("/Users/choeseunghui/Documents/allocation1.csv", 'w', newline="")
+            wr = csv.writer(writer_file)
+            wr.writerow([
+                '종목명', '배당수익률(%)'
                 ])
+            for i in range(len(self.name_List)):
+                wr.writerow([                 # len() : 문자열의 길이 구하기, 리스트에 들어있는 요소가 몇 개인지 알 수 있음
+                    self.name_List[i],
+                    self.revenue_List[i]
+                    ])
 
-        write_file.close()
+            writer_file.close()
